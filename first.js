a849f22d41cf134365da2ced0561c3d38510450a
Yajvendra <yajvendra98@gmail.com>
2024-01-06 01:44:25 +0530

    learning JavaScript Day-1

---
+++ first.js
@@ -0,0 +1,27 @@
+// Day-1 | JavaScript | Practice Problem - 1
+
+const product = {
+    productName: "Ball Pen",
+    productRating: 4,
+    productTag: "Deal of the Day",
+    productPrice: 270,
+    productOffer: "5% off"
+}
+
+console.log(product.productName);
+console.log(product.productOffer);
+console.log(product.productPrice);
+console.log(product.productRating);
+console.log(product.productTag);
+
+
+// Day-1 | JavaScript | Practice Problem - 2
+
+const instagramProfile = {
+    name: "Yajvendra Saini",
+    isFollow: true,
+    posts: 195,
+    followers: "596K",
+    following: 4,
+    bio: " One Stap at a Time!⚡"
+}   
