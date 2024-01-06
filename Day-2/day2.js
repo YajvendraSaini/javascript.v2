36f8379695f58997b7508319aaadd7d2533163d4
Yajvendra <yajvendra98@gmail.com>
2024-01-07 01:06:48 +0530

    Done with Operators

---
+++ Day-2/day2.js
@@ -0,0 +1,28 @@
+// Operator in JS
+// Arthimetic Operator
+
+let a = 5;
+let b = 2;
+
+console.log("a + b =", a+b);
+console.log("a - b =", a-b);
+console.log("a * b =", a*b);
+console.log("a / b =", a/b);
+console.log("a % b =", a%b);
+console.log("a + b =", a+b);
+console.log("a ** b =", a**b);
+
+
+// Unary Operators
+
+a = a+1;
+console.log("a = ", a++);
+
+// Assignment Operators
+
+a += 3
+console.log("a = ", a);
+
+
+
+
