612d11905541d20e203f7625cbc6703f32b3d3a6
Yajvendra <yajvendra98@gmail.com>
2024-01-07 01:34:13 +0530

    added CSS file

---
+++ Day-2/2.2 Conditions/D2-Conditions.js
@@ -0,0 +1,14 @@
+// Conditional Statements
+
+let mode = "dark";
+let color;
+
+if(mode === "dark"){
+    color = "black";
+}
+
+if (mode === "light"){
+    color = "light";
+}
+
+console.log(color);
