7a064677f6f5b74c70fb8c5bbb5bbdc6c3fcffe2
Yajvendra <yajvendra98@gmail.com>
2024-01-07 01:54:03 +0530

    else-if statement

--- Day-2/2.2 Conditions/D2-Conditions.js
+++ Day-2/2.2 Conditions/D2-Conditions.js
@@ -1,6 +1,8 @@
 // Conditional Statements
 
-let mode = "dark";
+// if statement
+
+let mode = "purple";
 let color;
 
 if(mode === "dark"){
@@ -12,3 +14,32 @@ if (mode === "light"){
 }
 
 console.log(color);
+
+
+// if-else statement
+
+let a = 10;
+
+if(a%2 === 0){
+    console.log( a, "is even");
+} else{
+    console.log(a, "is odd");
+}
+
+
+// else-if statement
+
+if(mode === "dark"){
+    color = "black";
+} else if(mode ==="purple"){
+    color = "purple";
+} else if(mode === "yellow"){
+    color = "yellow";
+} else {
+    color = "light";
+}
+
+console.log(color);
+
+
+
