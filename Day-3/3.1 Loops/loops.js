0c1d0faaf45f2b9754a0b6975b7ec53335475158
Yajvendra <yajvendra98@gmail.com>
2024-01-08 00:53:06 +0530

    for in loop

---
+++ Day-3/3.1 Loops/loops.js
@@ -0,0 +1,30 @@
+// Calculate sum of 1 to 5 using for Loop
+
+// let sum = 0;
+
+// for(let i=1; i<=5; i++){
+//     sum = sum + i;
+// }
+
+// console.log("sum =", sum);
+
+// //while loop
+
+// let i = 1;
+// while(i<=5){
+//     console.log("i =",i);
+//     i++;
+// }
+
+//for in loop
+
+let student = {
+    name: "Yajvendra Saini",
+    age: 20,
+    cgpa: 8,
+    isPass: true,
+};
+
+for (let i in student){
+    console.log(i);
+}
