67809f5cb46b99fa7077529d145766c529b1a70f
Yajvendra <yajvendra98@gmail.com>
2024-02-23 00:27:11 +0530

    arrays methods

---
+++ Day-4/4.2 Array Methods/array_methods.js
@@ -0,0 +1,24 @@
+
+
+let names = ["Yajvendra", "Ananya", "Prince", "Aanchal", "Ram"];
+console.log(names);
+
+names.push("Anshu", "Manshi");
+console.log(names);
+
+names.pop();
+console.log(names);
+
+console.log(names.toString());
+
+let anotherNames = ["Yajju", "Nishu", "Vijay", "Aachuu 🤧", "RAM", "Chiki", "missi"];
+
+console.log(names.concat(anotherNames));
+
+
+names.unshift("Mini Zoo");
+console.log(names);
+
+names.shift();
+console.log(names);
+
