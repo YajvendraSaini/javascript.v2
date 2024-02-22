872ba3ccc10de56a9bd4cbabb3b05c624e6a4862
Yajvendra <yajvendra98@gmail.com>
2024-02-22 23:23:51 +0530

    Array in JS

---
+++ Day-4/array.js
@@ -0,0 +1,16 @@
+let friend = ["Aancahl", "Anshu", "Mansi", "Ananya", "Prince", "Ram"];
+
+console.log(friend);
+
+
+//using for loop with array
+
+for(let idx = 0; idx <friend.length; idx++){
+    console.log(friend[idx]);
+}
+
+// using for of loop with array
+
+for(let el of friend){
+    console.log(el);
+}
