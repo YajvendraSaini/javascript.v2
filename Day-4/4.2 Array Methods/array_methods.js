ec40a28d20aacfa607bbf456d4a9aae5ba07b069
Yajvendra <yajvendra98@gmail.com>
2024-02-23 00:58:35 +0530

    array methods

--- Day-4/4.2 Array Methods/array_methods.js
+++ Day-4/4.2 Array Methods/array_methods.js
@@ -22,3 +22,26 @@ console.log(names);
 names.shift();
 console.log(names);
 
+console.log(names.slice(1,6));
+
+let num = [1,2,3,4,5,6,7];
+
+num.splice(2, 2, 10, 20);
+console.log(num);
+console.log(num.length);
+
+num.splice(2, 0, 10);
+console.log(num);
+console.log(num.length);
+
+num.splice(2, 1);
+console.log(num);
+console.log(num.length);
+
+num.splice(3);
+console.log(num);
+console.log(num.length);
+
+num.splice();
+console.log(num);
+console.log(num.length);
